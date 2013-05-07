84a24bbfda9a54db11bf00c55c3b761dfa606d35
Alice Boxhall <aboxhall@chromium.org>
2013-05-07 11:12:44 -0700

    Add new audit rules and AuditResults object

---
+++ src/audits/LinkWithUnclearPurpose.js
@@ -0,0 +1,33 @@
+// Copyright 2013 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+goog.require('axs.AuditRule');
+goog.require('axs.constants.Severity');
+
+/**
+ * @type {axs.AuditRule.Spec}
+ */
+axs.AuditRule.specs.linkWithUnclearPurpose = {
+    name: 'linkWithUnclearPurpose',
+    severity: axs.constants.Severity.WARNING,
+    relevantNodesSelector: function(scope) {
+        return scope.querySelectorAll('a');
+    },
+    test: function(anchor) {
+        // This only looks for "click here" in a link's content. Not
+        // comprehensive, but should catch the worst case.
+        return (/^\s*click\s*here\s*[^a-z]?$/i).test(anchor.textContent);
+    },
+    code: 'AX_TITLE_01'
+};
