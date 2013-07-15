a32a183662d0d4329705dcb595b7e8b1c579fccb
Cameron Cundiff and Trevor John <pair+ccundiff+tjohn@pivotallabs.com>
2013-07-15 16:18:58 -0400

    Adding jsdoc comments.

--- src/js/BrowserUtils.js
+++ src/js/BrowserUtils.js
@@ -1,5 +1,12 @@
 goog.provide('axs.browserUtils');
 
+/**
+ * Use Firefox matcher when Webkit is not supported.
+ * @constructor
+ * @param {Node} node
+ * @param {String} selector
+ * @returns {Boolean}
+ */
 axs.browserUtils.matchSelector = function(node, selector) {
   if(node.webkitMatchesSelector){
     return node.webkitMatchesSelector(selector);
