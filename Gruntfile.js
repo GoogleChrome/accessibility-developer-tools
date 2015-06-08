fbcff09418278a890aa17e787dd0ebc1d88e4f45
Xavier Spriet <linkadmin@gmail.com>
2015-06-08 10:42:22 -0400

    Gruntfile cleanups
    * Normalize string quoting.
    * Move build path for `axs_testing.js` to `.tmp/build/`

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,28 +11,28 @@ module.exports = function(grunt) {
       minify: {
         requiresConfig: 'git-revision',
         files: {
-          ".tmp/build/axs_testing.js": [
-              "./lib/closure-library/closure/goog/base.js",
-              "./src/js/axs.js",
-              "./src/js/BrowserUtils.js",
-              "./src/js/Constants.js",
-              "./src/js/AccessibilityUtils.js",
-              "./src/js/Properties.js",
-              "./src/js/AuditRule.js",
-              "./src/js/AuditRules.js",
-              "./src/js/AuditResults.js",
-              "./src/js/Audit.js",
-              "./src/audits/*"
+          '.tmp/build/axs_testing.js': [
+              './lib/closure-library/closure/goog/base.js',
+              './src/js/axs.js',
+              './src/js/BrowserUtils.js',
+              './src/js/Constants.js',
+              './src/js/AccessibilityUtils.js',
+              './src/js/Properties.js',
+              './src/js/AuditRule.js',
+              './src/js/AuditRules.js',
+              './src/js/AuditResults.js',
+              './src/js/Audit.js',
+              './src/audits/*'
           ]
         },
         options: {
-          "language_in": "ECMASCRIPT5",
-          "formatting": "PRETTY_PRINT",
-          "summary_detail_level": 3,
-          "warning_level": "VERBOSE",
-          "compilation_level": "SIMPLE_OPTIMIZATIONS",
-          "output_wrapper": "<%= grunt.file.read('scripts/output_wrapper.txt') %>",
-          "externs": "./src/js/externs/externs.js"
+          'language_in': 'ECMASCRIPT5',
+          'formatting': 'PRETTY_PRINT',
+          'summary_detail_level': 3,
+          'warning_level': 'VERBOSE',
+          'compilation_level': 'SIMPLE_OPTIMIZATIONS',
+          'output_wrapper': "<%= grunt.file.read('scripts/output_wrapper.txt') %>",
+          'externs': './src/js/externs/externs.js'
         }
       }
     },
@@ -56,12 +56,12 @@ module.exports = function(grunt) {
     var done = this.async();
 
     grunt.util.spawn({
-      "cmd" : "git",
-      "args" : [ "rev-parse", "HEAD" ],
-      "opts" : {
-        "cwd" : "."
+      'cmd' : 'git',
+      'args' : [ 'rev-parse', 'HEAD' ],
+      'opts' : {
+        'cwd' : '.'
       }
-      }, function(err, result) {
+    }, function(err, result) {
       if (err) {
           grunt.log.error(err).verbose.error(result);
           done();
@@ -75,7 +75,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('save-revision', function() {
     grunt.event.once('git-describe', function (rev) {
-      grunt.log.writeln("Git Revision: " + rev);
+      grunt.log.writeln('Git Revision: ' + rev);
       grunt.config.set('git-revision', rev);
     });
     grunt.task.run('git-describe');
