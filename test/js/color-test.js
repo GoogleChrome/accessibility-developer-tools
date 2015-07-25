ca116fc31229e3ffa078077bc12ca1f6050bc272
Rick <ricksbrown73@gmail.com>
2015-07-26 07:34:34 +1000

    Added unit tests for transparent background to prevent regressions.

--- test/js/color-test.js
+++ test/js/color-test.js
@@ -43,6 +43,25 @@ test("parses alpha values correctly", function() {
   equal(color.alpha, .47);
 });
 
+test("handles rgba transparent value correctly", function() {
+  var colorString = 'rgba(0, 0, 0, 0)';
+  var color = axs.color.parseColor(colorString);
+  equal(color.red, 0);
+  equal(color.blue, 0);
+  equal(color.green, 0);
+  equal(color.alpha, 0);
+});
+
+test("handles xbrowser 'transparent' value correctly", function() {
+  // Firefox, IE11, Project Spartan (MS Edge Release Candidate)
+  var colorString = 'transparent';
+  var color = axs.color.parseColor(colorString);
+  equal(color.red, 0);
+  equal(color.blue, 0);
+  equal(color.green, 0);
+  equal(color.alpha, 0);
+});
+
 module("suggestColors");
 test("suggests correct grey values", function() {
   var white = new axs.color.Color(255, 255, 255, 1)
