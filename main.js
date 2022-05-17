c6dba722d1da0c559d4379eac029c7255211c003
Harphuna <86975697+Hakunasc11@users.noreply.github.com>
2022-05-17 11:44:10 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// This exposes the ./dist Javascript file for node libraries.
-// It also unwraps the main axs package so Audit and other objects are exposed
-// directly in the node library
-
-var library = require('./dist/js/axs_testing'); // eslint-disable-line no-undef
-
-module.exports = library.axs; // eslint-disable-line no-undef
